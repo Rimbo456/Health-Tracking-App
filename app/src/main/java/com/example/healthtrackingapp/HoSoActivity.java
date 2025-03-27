890960635493f6e2e0e29a2224fbcefc02ed859b
Nhut151104 <feniknhut151104@gmail.com>
2025-03-27 16:36:12 +0700

    update

--- app/src/main/java/com/example/healthtrackingapp/HoSoActivity.java
+++ app/src/main/java/com/example/healthtrackingapp/HoSoActivity.java
@@ -13,6 +13,8 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,7 +26,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class HoSoActivity extends Activity {
+public class HoSoActivity extends AppCompatActivity {
     Boolean gender;
     long birthYear;
     double height, weight;
@@ -40,6 +42,7 @@ public class HoSoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ho_so);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_ho_so), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
--- app/src/main/java/com/example/healthtrackingapp/HoTroVaThongTinActivity.java
+++ app/src/main/java/com/example/healthtrackingapp/HoTroVaThongTinActivity.java
@@ -1,8 +1,12 @@
 package com.example.healthtrackingapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +14,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HoTroVaThongTinActivity extends Activity {
+public class HoTroVaThongTinActivity extends AppCompatActivity {
 
     ImageView imgQuayLai;
+    Button btnHoTro, btnBaoMat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ho_tro_va_thong_tin);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_ho_tro_va_thong_tin), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -25,7 +31,22 @@ public class HoTroVaThongTinActivity extends Activity {
         });
 
         imgQuayLai = findViewById(R.id.imgQuayLai);
+        btnHoTro = findViewById(R.id.btnHoTro);
+        btnBaoMat = findViewById(R.id.btnBaoMat);
 
         imgQuayLai.setOnClickListener(view -> finish());
+
+        btnHoTro.setOnClickListener(v -> {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+            emailIntent.setData(Uri.parse("mailto:nguyenminhnhut151104@gmail.com"));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hỗ trợ ứng dụng");
+            startActivity(Intent.createChooser(emailIntent, "Chọn ứng dụng email"));
+        });
+
+        btnBaoMat.setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://yourapp.com/privacy"));
+            Toast.makeText(HoTroVaThongTinActivity.this, "Chưa có", Toast.LENGTH_SHORT);
+            startActivity(browserIntent);
+        });
     }
 }
