1a0b6a8c2a62d815e77a98d5a858e064a8af98a1
Nhut151104 <feniknhut151104@gmail.com>
2025-04-01 07:41:34 +0700

    bao thức

--- app/src/main/java/com/example/healthtrackingapp/ui/screens/UserScreen.kt
+++ app/src/main/java/com/example/healthtrackingapp/ui/screens/UserScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import com.example.healthtrackingapp.BaoThucActivity
 import com.example.healthtrackingapp.GioiThieuActivity
 import com.example.healthtrackingapp.HoSoActivity
 import com.example.healthtrackingapp.HoTroVaThongTinActivity
@@ -77,7 +78,7 @@ fun UserScreen() {
             }
 
             tbrVeChungToi.setOnClickListener {
-                val intent = Intent(context, GioiThieuActivity::class.java)
+                val intent = Intent(context, BaoThucActivity::class.java)
                 context.startActivity(intent)
             }
             view
